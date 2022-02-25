79bb4f3f5f5372e5fa13f68f9e4e430a097dde8a
CharlesZhu <charleszhuren@gmail.com>
2022-02-25 18:52:42 +0800

    Initial commit

---
+++ Hello.go
@@ -0,0 +1,8 @@
+package main  // 包声明
+
+import "fmt"  // 引入包
+
+func main(){  // 函数
+	var a = "Hello World! \nfrom 2019.9.22 to 2022.2.17"
+	fmt.Println(a)
+}
---
+++ Hello2.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("希望你能从容淡定 充实幸福地过好每一天")
+}

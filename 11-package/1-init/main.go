3258b6760b9c3931c7817c647438a7aafa40638c
CharlesZhu <charleszhuren@gmail.com>
2022-03-06 14:45:29 +0800

    update package knowledge

---
+++ 11-package/1-init/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"GoLearning/11-package/1-init/utils"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("main run")
+	utils.Somefunc()
+}
---
+++ 11-package/1-init/utils/tool.go
@@ -0,0 +1,15 @@
+package utils
+
+import "fmt"
+
+func init() {
+	fmt.Println("tool init")
+}
+
+func init() {
+	fmt.Println("init again")
+}
+
+func Somefunc() {
+	fmt.Println("This is some func")
+}
